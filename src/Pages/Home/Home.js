a2a3b79b98feb230ebaf4ea83ec21fcafe6a57d4
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-10 11:32:03 +0600

    banner done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <h2>I'm home</h2>
         </div>
     );

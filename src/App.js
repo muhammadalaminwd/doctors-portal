39beb896f1ae9d49de5259804b1f02f575892414
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-10 10:31:02 +0600

    login component done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './Pages/Shared/Navbar';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div>
     <Navbar></Navbar>
     <Routes>
+    <Route path='/' element={<Home />}></Route>
+    <Route path='/about' element={<About />}></Route>
+    <Route path='login' element={<Login></Login>}></Route>
     </Routes>
+
     </div>
   );
 }

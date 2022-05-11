870b9ecd82e633c86bf2ad396555c1df362050aa
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-11 14:51:42 +0600

    available appointment card done

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,12 +1,32 @@
-import { format } from 'date-fns';
-import React from 'react';
+import { format } from "date-fns";
+import React, { useEffect, useState } from "react";
+import Service from "./Service";
 
-const AvailableAppointment = ({date}) => {
-    return (
-        <div>
-        <h4 className='text-xl text-secondary text-center'>Available Appointments on {format(date, 'PP')}</h4>
-        </div>
-    );
+const AvailableAppointment = ({ date }) => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div>
+      <h4 className="text-xl text-secondary text-center">
+        Available Appointments on {format(date, "PP")}
+      </h4>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      {
+        services.map(service => <Service
+            key={service}
+            service = {service}
+            >
+            </Service>)
+      }
+      </div>
+    </div>
+  );
 };
 
 export default AvailableAppointment;

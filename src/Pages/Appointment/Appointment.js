39cf7629f023af810c9c38d061c8e408a1e51c58
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-11 16:27:31 +0600

    modal added

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Footer from '../Shared/Footer';
 import AppointmentBanner from './AppointmentBanner';
 import AvailableAppointment from './AvailableAppointment';
+import BookingModal from './BookingModal';
 
 const Appointment = () => {
     const [date, setDate] = useState(new Date ());
--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,9 +1,11 @@
 import { format } from "date-fns";
 import React, { useEffect, useState } from "react";
+import BookingModal from "./BookingModal";
 import Service from "./Service";
 
 const AvailableAppointment = ({ date }) => {
   const [services, setServices] = useState([]);
+  const [treatment, setTreatment] = useState(null);
 
   useEffect(() => {
     fetch("services.json")
@@ -21,10 +23,17 @@ const AvailableAppointment = ({ date }) => {
         services.map(service => <Service
             key={service}
             service = {service}
+            setTreatment = {setTreatment}
             >
             </Service>)
       }
       </div>
+      {treatment && <BookingModal
+        date={date}
+        treatment={treatment}
+        setTreatment={setTreatment}
+        >
+        </BookingModal>}
     </div>
   );
 };

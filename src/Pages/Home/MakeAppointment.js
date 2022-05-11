a24ca97bf9aeba754142c110b7a1639d357f452a
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-11 12:47:33 +0600

    appointment banner page created

--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -11,10 +11,10 @@ const MakeAppointment = () => {
             <div className='flex-1 hidden lg:block'>
             <img className='mt-[-150px]' src={doctor} alt="" />
             </div>
-            <div className='flex-1'>
+            <div className='flex-1 px-5'>
             <h3 className='text-xl text-primary font-bold'>Appointment</h3>
-            <h2 className='text-3xl text-white'>Make an Appointment Today</h2>
-            <p className='text-white'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat, consequatur accusamus culpa, at porro officia eius recusandae similique officiis rem corrupti quibusdam quasi? Labore praesentium facere vero sint soluta, asperiores, sed porro nisi adipisci mollitia sequi similique excepturi maxime inventore!</p>
+            <h2 className='text-3xl text-white py-5'>Make an Appointment Today</h2>
+            <p className='text-white pb-5'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat, consequatur accusamus culpa, at porro officia eius recusandae similique officiis rem corrupti quibusdam quasi? Labore praesentium facere vero sint soluta, asperiores, sed porro nisi adipisci mollitia sequi similique excepturi maxime inventore!</p>
             <PrimaryButton>GET STARTED</PrimaryButton>
             </div>
 

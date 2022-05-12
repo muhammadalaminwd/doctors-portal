d494ed27a314d11b9997d45bab716faf162758ee
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-12 17:26:57 +0600

    appointment protected done

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { format } from "date-fns";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 
 const BookingModal = ({date, treatment, setTreatment}) => {
     const {_id, name, slots} = treatment;
+    const [user, loading, error] = useAuthState(auth);
 
     const handleBooking = event => {
         event.preventDefault();
@@ -28,8 +31,8 @@ const BookingModal = ({date, treatment, setTreatment}) => {
                 slots.map(slot => <option value={slot}>{slot}</option>)
             }
           </select>
-          <input type="text" name="name" placeholder="Your Name" className="input input-bordered w-full max-w-xs" />
-            <input type="email" name="email" placeholder="Email Address" className="input input-bordered w-full max-w-xs" />
+          <input type="text" name="name" disabled value={user?.displayName} className="input input-bordered w-full max-w-xs" />
+            <input type="email" name="email" disabled value={user?.email} className="input input-bordered w-full max-w-xs" />
             <input type="text" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
             <input type="submit" value = "Submit" placeholder="Type here" className="btn btn-secondary" />
             </form>

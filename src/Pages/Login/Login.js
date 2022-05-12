d89b4826d3536d710109328c3521b82e1e4d53ab
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-12 18:00:33 +0600

    enough coding for today

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
@@ -27,6 +27,13 @@ const Login = () => {
   const location = useLocation();
   let from = location.state?.from?.pathname || "/";
 
+  useEffect( () => {
+    if (user || gUser) {
+        navigate(from, {replace: true});
+    
+      }
+  }, [user, gUser, from, navigate])
+
   if(loading || gLoading){
       return <Loading></Loading>
   }
@@ -35,12 +42,8 @@ const Login = () => {
       signInError = <p className="text-red-500"><small>{error?.message || gError?.message}</small></p>
   }
 
-  if (user || gUser) {
-    navigate(from, {replace: true});
 
-  }
   const onSubmit = (data) => {
-  console.log(data);
   signInWithEmailAndPassword(data.email, data.password)
   }
   return (
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
         <Link to="/about">About</Link>
       </li>
       <li>
-        {user ? <button class="btn btn-ghost" onClick={logout}>Sign Out</button>
+        {user ? <button className="btn btn-ghost" onClick={logout}>Sign Out</button>
         : <Link to="/login">Login</Link>}
       </li>
     </>

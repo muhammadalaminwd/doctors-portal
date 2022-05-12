b5cd32941c2bc40d5aa5d0b148460a32e2921fc4
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-12 16:39:36 +0600

    sign up created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Login from './Pages/Login/Login';
 import Appointment from './Pages/Appointment/Appointment';
+import SignUp from './Pages/Login/SignUp';
+import RequireAuth from './Pages/Login/RequireAuth';
 
 function App() {
   return (
@@ -13,8 +15,13 @@ function App() {
     <Routes>
     <Route path='/' element={<Home />}></Route>
     <Route path='/about' element={<About />}></Route>
-    <Route path='login' element={<Login></Login>}></Route>
-    <Route path='appointment' element={<Appointment />}></Route>
+    <Route path='login' element={<Login />}></Route>
+    <Route path='signup' element={<SignUp />}></Route>
+    <Route path='appointment' element={
+      <RequireAuth>
+      <Appointment />
+      </RequireAuth>}>
+      </Route>
     </Routes>
 
     </div>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,6 +3,7 @@ import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-fireba
 import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
 import Loading from "../Shared/Loading";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -30,8 +31,9 @@ const Login = () => {
       signInError = <p className="text-red-500"><small>{error?.message || gError?.message}</small></p>
   }
 
-  if (gUser) {
-    console.log(gUser);
+  if (user || gUser) {
+    console.log(user || gUser);
+
   }
   const onSubmit = (data) => {
   console.log(data);
@@ -96,7 +98,8 @@ const Login = () => {
                 {signInError}
             <input className="btn w-full max-w-xs text-white" type="submit" value="Login"/>
           </form>
-
+                <p className="text-center"><small>New to Doctors Portal? <Link className="text-primary" to="/signup">Create a New Account</Link></small>
+                </p>
           <div className="divider">OR</div>
 
           <button

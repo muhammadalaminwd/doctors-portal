e4d43f9bc7c459e368edf0a0628cccde88ba0a1b
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-17 19:37:35 +0600

    token issued

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,6 +4,7 @@ import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
 import Loading from "../Shared/Loading";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import useToken from "../../hooks/useToken";
 
 const Login = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -21,6 +22,8 @@ const Login = () => {
     error,
   ] = useSignInWithEmailAndPassword(auth);
 
+  const [token] = useToken(user || user);
+
   let signInError;
 
   const navigate = useNavigate();
@@ -28,11 +31,11 @@ const Login = () => {
   let from = location.state?.from?.pathname || "/";
 
   useEffect( () => {
-    if (user || gUser) {
+    if (token) {
         navigate(from, {replace: true});
     
       }
-  }, [user, gUser, from, navigate])
+  }, [token, from, navigate])
 
   if(loading || gLoading){
       return <Loading></Loading>

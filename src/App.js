952f8edc70c91d5c81943aed844422d53175e2c2
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-19 13:06:04 +0600

    add a new doctor component added

--- src/App.js
+++ src/App.js
@@ -13,31 +13,32 @@ import Dashboard from './Pages/Dashboard/Dashboard';
 import MyAppointment from './Pages/Dashboard/MyAppointment';
 import MyReview from './Pages/Dashboard/MyReview';
 import MyHistory from './Pages/Dashboard/MyHistory';
-import Users from './Pages/Dashboard/Users';
+import AddDoctor from './Pages/Dashboard/AddDoctor';
 import RequireAdmin from './Pages/Login/RequireAdmin';
+import Users from './Pages/Dashboard/Users';
 function App() {
   return (
     <div className='max-w-7xl mx-auto px-12'>
     <Navbar></Navbar>
     <Routes>
-    <Route path='/' element={<Home />}></Route>
-    <Route path='/about' element={<About />}></Route>
-    <Route path='login' element={<Login />}></Route>
-    <Route path='signup' element={<SignUp />}></Route>
+    <Route path='/' element={<Home />} />
+    <Route path='/about' element={<About />} />
     <Route path='appointment' element={
       <RequireAuth>
       <Appointment />
-      </RequireAuth>}>
-      </Route>
+      </RequireAuth>
+    } />
     <Route path='dashboard' element={<RequireAuth><Dashboard /></RequireAuth>}>
       <Route index element={<MyAppointment></MyAppointment>}></Route>
       <Route path='review' element={<MyReview></MyReview>}></Route>
       <Route path='history' element={<MyHistory></MyHistory>}></Route>
-      <Route path='users' element={<RequireAdmin></RequireAdmin>}></Route>
+      <Route path='users' element={<RequireAdmin><Users></Users></RequireAdmin>}></Route>
+      <Route path='addDoctor' element={<RequireAdmin><AddDoctor></AddDoctor></RequireAdmin>}></Route>
       </Route>
+      <Route path='login' element={<Login />} />
+      <Route path='signup' element={<SignUp />} />
     </Routes>
     <ToastContainer />
-
     </div>
   );
 }

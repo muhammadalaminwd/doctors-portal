5bd0965e43e10b90aab56dfd88e5d48ac0435891
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-19 18:34:53 +0600

    image hosting done

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -40,7 +40,6 @@ const SignUp = () => {
     await createUserWithEmailAndPassword(data.email, data.password);
     await updateProfile({ displayName: data.name });
     console.log('update done')
-    
     }
 
     return (
